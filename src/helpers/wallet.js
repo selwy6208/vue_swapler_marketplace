610083bc03f39800f965120ce857256e3cd16664
strong6208 <hamilton6208@gmail.com>
2022-11-08 17:54:57 +0300

    Blockchain communication functions

--- src/helpers/wallet.js
+++ src/helpers/wallet.js
@@ -19,31 +19,221 @@ async function connectKeeper() {
         }
     }
 }
+/**
+ * 
+ * @param { String } funcName Name of the function to invoke 
+ * @param { Array } args Arguments for the function [{ type, value }]
+ * @param { Object[] } payment Payment Object [{ assetId, amount }]
+ * @returns 
+ */
+async function invokeFunction(
+    funcName,
+    args,
+    payment,
 
-async function invokeOffer(contractAddr) {
+) {
     if (window.signer) {
-        const invokation = await window.signer.invoke({
-            dApp: contractAddr,
-            fee: 0,
-            payment: [
-                // 10 max
-                {
-                    assetId: '',
-                    amount: 0,
-                },
-            ],
+        const invocation = await window.signer.invoke({
+            dApp: window.contractAddress,
+            fee: 900000, // TODO: check
+            payment,
             call: {
-                function: 'offer', // TODO
-                args: [
-                    // {
-                    //     type: '', // 'integer' | 'string' | 'binary'
-                    //     value: '' // number | 'string'
-                    // }
-                ],
+                'function': funcName,
+                args,
             },
         });
-        return invokation;
+        return invocation;
+    } else {
+        throw new Error("signer not found");
     }
 }
 
-export { connectKeeper, invokeOffer };
+/**
+ * Sale asset for waves with given price
+ * @param { String } assetId Asset id
+ * @param { Number } price 
+ */
+async function offerForSale(assetId, price) {
+    const args = [
+        {
+            type: 'string',
+            value: 'WAVES'
+        },
+        {
+            type: 'integer',
+            value: price * Math.pow(10, 8)
+        }
+    ];
+    const payment = [
+        {
+            assetId,
+            amount: 1,
+        }
+    ];
+    try {
+        const invocation = await invokeFunction(
+            'offerForSale',
+            args,
+            payment
+        );
+        const response = await invocation.broadcast();
+        return { 
+            response,
+            error: undefined
+        };
+    } catch(error) {
+        return {
+            response: undefined,
+            error
+        }
+    }
+}
+
+/**
+ * Make an offer on asset
+ * @param { String } wantAssetId
+ * @param { Object[] } payment What user want to swap on wantAssetId
+ */
+async function offerForSwap(wantAssetId, payment) {
+    const args = [
+        {
+            type: 'string',
+            value: wantAssetId
+        }
+    ];
+    try {
+        const invocation = await invokeFunction(
+            'offerForSwap',
+            args,
+            payment
+        );
+        const response = await invocation.broadcast();
+        return { 
+            response,
+            error: undefined
+        };
+    } catch(error) {
+        return {
+            response: undefined,
+            error
+        };
+    }
+}
+
+// instant buy
+async function buy(assetId, payment) {
+    const args = [
+        {
+            type: 'string',
+            value: assetId
+        }
+    ];
+    try {
+        const invocation = await invokeFunction(
+            'buy',
+            args,
+            payment
+        );
+        const response = await invocation.broadcast();
+        return { 
+            response,
+            error: undefined
+        };
+    } catch(error) {
+        return {
+            response: undefined,
+            error
+        };
+    }
+}
+// confirm offer
+async function swapDone(offerId) {
+    const args = [
+        {
+            type: 'string',
+            value: offerId
+        }
+    ];
+    const payment = [];
+    try {
+        const invocation = await invokeFunction(
+            'swapDone',
+            args,
+            payment
+        );
+        const response = await invocation.broadcast();
+        return { 
+            response,
+            error: undefined
+        };
+    } catch(error) {
+        return {
+            response: undefined,
+            error
+        };
+    }
+}
+
+// cancel selling nft
+async function cancelSelling(assetId) {
+    const args = [
+        {
+            type: 'string',
+            value: assetId
+        }
+    ];
+    const payment = [];
+    try {
+        const invocation = await invokeFunction(
+            'swapDone',
+            args,
+            payment
+        );
+        const response = await invocation.broadcast();
+        return { 
+            response,
+            error: undefined
+        };
+    } catch(error) {
+        return {
+            response: undefined,
+            error
+        }
+    }
+}
+
+async function swapCancel(offerId) {
+    const args = [
+        {
+            type: 'string',
+            value: offerId
+        }
+    ];
+    const payment = [];
+    try {
+        const invocation = await invokeFunction(
+            'swapCancel',
+            args,
+            payment
+        );
+        const response = await invocation.broadcast();
+        return { 
+            response,
+            error: undefined
+        };
+    } catch(error) {
+        return {
+            response: undefined,
+            error
+        };
+    }
+}
+
+export { 
+    connectKeeper,
+    offerForSale,
+    offerForSwap,
+    buy,
+    swapDone,
+    swapCancel
+};
